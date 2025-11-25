a13659f67ed62fd1b6508b13c35320e94b4c194e
ShunOyagi <c0a24053f0@edu.teu.ac.jp>
2025-11-25 16:58:10 +0900

    演習問題2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -61,17 +61,49 @@ def gameover(screen: pg.Surface) -> None:  # 演習問題1
     pg.time.wait(5000)
 
 
+def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:  #演習問題2
+    bb_imgs = []
+    bb_accs = [a for a in range(1, 11)]
+    for r in range(1, 11):
+        bb_img = pg.Surface((20*r, 20*r))
+        bb_img.set_colorkey((0, 0, 0))
+        pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+        bb_imgs.append(bb_img)
+        #bb_accs.append(r)
+    return bb_imgs, bb_accs
+
+
+def get_kk_imgs() -> dict[tuple[int,int], pg.Surface]:  #演習問題3
+    base_img = pg.image.load("fig/3.png")
+    kk_imgs = {
+        ( 0,  0): pg.transform.rotozoom(base_img, 0, 0.9),    # 静止
+        (+5,  0): pg.transform.rotozoom(base_img, -90, 0.9),  # 右
+        (+5, -5): pg.transform.rotozoom(base_img, -45, 0.9),  # 右上
+        ( 0, -5): pg.transform.rotozoom(base_img, 0, 0.9),    # 上
+        (-5, -5): pg.transform.rotozoom(base_img, 45, 0.9),   # 左上
+        (-5,  0): pg.transform.rotozoom(base_img, 90, 0.9),   # 左
+        (-5, +5): pg.transform.rotozoom(base_img, 135, 0.9),  # 左下
+        ( 0, +5): pg.transform.rotozoom(base_img, 180, 0.9),  # 下
+        (+5, +5): pg.transform.rotozoom(base_img, -135, 0.9), # 右下
+    }
+    return kk_imgs
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
-    bg_img = pg.image.load("fig/pg_bg.jpg")    
-    kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
-    kk_rct = kk_img.get_rect()
-    kk_rct.center = 300, 200
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    kk_imgs = get_kk_imgs()  #演習問題3
+    kk_img = kk_imgs[(0,0)]  #演習問題3
+    #kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
+    kk_rct = kk_img.get_rect(center=(300, 200))  #演習問題3
+    # kk_rct = kk_img.get_rect()
+    # kk_rct.center = 300, 200
     bb_img = pg.Surface((20, 20))  #練習問題2  空のSurface
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  #練習問題2
     bb_img.set_colorkey((0, 0, 0))  #練習問題2
+    bb_imgs, bb_accs = init_bb_imgs()  #演習問題2
+    bb_index = 0  #演習問題2
     bb_rct = bb_img.get_rect()  #練習問題2  爆弾Rect
     bb_rct.centerx = random.randint(0, WIDTH)  #練習問題2
     bb_rct.centery = random.randint(0, HEIGHT)  #練習問題2
@@ -111,13 +143,40 @@ def main():
         kk_rct.move_ip(sum_mv)
         if check_bound(kk_rct) != (True, True):  #練習問題2  画面外なら
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])  #練習問題2  移動しなかったことにする
+        kk_img = kk_imgs.get(tuple(sum_mv), kk_imgs[(0,0)])  #演習問題3
         screen.blit(kk_img, kk_rct)  #練習問題2
-        yoko, tate = check_bound(bb_rct)  #練習問題3
-        if not yoko:  #練習問題3
-            vx *= -1  #練習問題3
-        if not tate:  #練習問題3
-            vy *= -1  #練習問題3
-        bb_rct.move_ip(vx, vy)  #練習問題2
+        
+        
+        # kk_img = kk_imgs[tuple(sum_mv)]  #演習問題3
+        # screen.blit(kk_img, kk_rct)
+        
+
+        # avx = vx * bb_accs[min(tmr // 500, 9)]  #演習問題2
+        # avy = vy * bb_accs[min(tmr // 500, 9)]
+        # bb_rct.move_ip(avx, avy)
+        # bb_img = bb_imgs[min(tmr // 500, 9)]
+        # bb_rct.width = bb_img.get_rect().width
+        # bb_rct.height = bb_img.get_rect().height
+        # yoko, tate = check_bound(bb_rct)  #練習問題3
+        # if not yoko:  #練習問題3
+        #     vx *= -1  #練習問題3
+        # if not tate:  #練習問題3
+        #     vy *= -1  #練習問題3
+
+
+        idx = min(tmr // 500, 9)  #演習問題2
+        bb_img = bb_imgs[idx]
+        center = bb_rct.center
+        bb_rct = bb_img.get_rect(center=center)
+        bb_rct.move_ip(vx * bb_accs[idx], vy * bb_accs[idx])
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
+
+
+        #bb_rct.move_ip(vx, vy)  #練習問題2
         screen.blit(bb_img, bb_rct)  #練習問題2
         pg.display.update()
         tmr += 1
